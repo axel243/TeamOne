44b77cd9d2d170babdc9ef28162d96043cb2bba6
ameliedehaan <amedehaan@gmail.com>
2019-10-11 14:15:25 +0200

    uitlezendata

--- js/uitlezendata.js
+++ js/uitlezendata.js
@@ -7,18 +7,14 @@ console.log(localStorage.getItem("Key"));
 
 // derde pagina met waarden die worden uitgelezen
 
-rijennum = 10;
-colomnum = 2;
-woordcolom = 50;
-defcolom = 200;
-
 var array = JSON.parse(localStorage.getItem("Key"));
 
 function generate_table() {
 
     var table = '<table>';
     for (var x = 0; x < array.length; x++) {
-        table += '<tr><td>' + array[x].woord + '</td><td>' + array[x].definitie + '</td></tr>';
+        table += '<tr><td>' + array[x].woord + '</td><td>'
+            + array[x].definitie + '</td></tr>';
         
     }
     table += '</table>';
@@ -27,19 +23,6 @@ function generate_table() {
     $('#table').html(table);
 }
 
-generate_table();
-
-
-
-// create a table
-
-//var Container = document.createElement("table");
-//var Woord = document.createElement("td");
-//var Definitie = document.createElement("td");
-//var Rijsamen = document.createElement("tr");
-
-//Container.appendChild(Rijsamen.appendChild(Woord, Definitie));
-
-
-
-
+$(Document).ready(function () {
+    generate_table();
+});
