5d40d8ebb2bff1aa0e8eb1b894deeab65f71806d
ameliedehaan <amedehaan@gmail.com>
2019-10-11 12:44:53 +0200

    tijdelijk log

---
+++ js/uitlezendata.js
@@ -0,0 +1,46 @@
+﻿// getting the data
+
+//console.log(JSON.parse(localStorage.getItem("Key")));
+console.log(localStorage.getItem("Key"));
+
+// derde pagina met waarden die worden uitgelezen
+
+rijennum = 10;
+colomnum = 2;
+woordcolom = 50;
+defcolom = 200;
+
+var array = [
+    {
+        woord: "test",
+        definitie: "test"
+    },
+    {
+        woord: "test2",
+        definitie: "test2",
+    }
+
+]
+
+function generate_table() {
+
+    var table = '<table>';
+    for (var i = 0; i < this.y; i++);
+    table_body += '<tr>';
+    for (var j = 0; j < this.x; j++);
+        
+}
+
+
+// create a table
+
+//var Container = document.createElement("table");
+//var Woord = document.createElement("td");
+//var Definitie = document.createElement("td");
+//var Rijsamen = document.createElement("tr");
+
+//Container.appendChild(Rijsamen.appendChild(Woord, Definitie));
+
+
+
+
