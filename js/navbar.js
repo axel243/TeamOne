b6809fd856012316305c45b2461de3a5a7c8b241
juless89 <j.m.r.kramer@gmail.com>
2019-10-11 15:55:39 +0200

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -6,6 +6,6 @@
 // });
 
 $(Document).ready(function () {
-    var string = '<navid="navigatie"><ul><li><!--linkone--><ahref="index.html">Startgame</a></li><li><!--linktwo--><ahref="#inputwoord.html">Inputwoord</a></li><li><!--linkthree--><ahref="hetSpel.html">Woordenlijst</a></li><li><!--linkfour--><ahref="#aboutUs">Aboutus</a></li></ul></nav>'
+    var string = '<navid="navigatie"><ul><li><!--linkone--><a href="index.html">Startgame</a></li><li><!--linktwo--><a href="#inputwoord.html">Inputwoord</a></li><li><!--linkthree--><a href="hetSpel.html">Woordenlijst</a></li><li><!--linkfour--><a href="#aboutUs">Aboutus</a></li></ul></nav>'
     $("#navbars").html(string);
 });
