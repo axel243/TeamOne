21f4315a2eb8de341d637d14545924743742010a
KoenSchnaar <koenschnaar@gmail.com>
2019-10-11 15:35:48 +0200

    4 random antwoorden

--- js/definitiewoorddisplay.js
+++ js/definitiewoorddisplay.js
@@ -1,23 +1,23 @@
 var woordJSON = localStorage.getItem("Key");
 var woordEnDefinities = JSON.parse(woordJSON);
-var arrayAantallen = Array.from(Array(woordEnDefinities.length).keys());
-console.log(arrayAantallen);
-
-// console.log(array);
-console.log(Math.floor(Math.random() * woordEnDefinities.length));
-
-
 
 function definitieEnWoord(){
-    var deVraag = woordEnDefinities[Math.floor(Math.random() * woordEnDefinities.length)];
-    var randomWoord1 = woordEnDefinities[Math.floor(Math.random() * woordEnDefinities.length)].woord;
-    var randomWoord2 = woordEnDefinities[Math.floor(Math.random() * woordEnDefinities.length)].woord;
-    var randomWoord3 = woordEnDefinities[Math.floor(Math.random() * woordEnDefinities.length)].woord;
+    
+    var arrayAantallen = Array.from(Array(woordEnDefinities.length).keys());
+    console.log(arrayAantallen);
 
-    document.getElementById("deDefinitie").innerHTML = deVraag.definitie;
-    document.getElementById("woord1").innerHTML = deVraag.woord;
-    document.getElementById("woord2").innerHTML = randomWoord1;
-    document.getElementById("woord3").innerHTML = randomWoord2;
-    document.getElementById("woord4").innerHTML = randomWoord3;
+
+    for (var x = 0; x < 4; x++) {
+        var random = Math.floor(Math.random() * arrayAantallen.length);
+        console.log(woordEnDefinities[arrayAantallen[random]].woord);
+        document.getElementById("woord" + (x+1)).innerHTML = woordEnDefinities[arrayAantallen[random]].woord;
+        arrayAantallen.splice(random, 1);
+
+        // console.log(x, random, arrayAantallen);
+    }
 
  }
+
+
+
+
