1e12734580e6e0f1144f3283e8f5a3724dee4da3
KoenSchnaar <koenschnaar@gmail.com>
2019-10-11 16:07:48 +0200

    random antwoorden plus definitie

--- js/definitiewoorddisplay.js
+++ js/definitiewoorddisplay.js
@@ -4,18 +4,24 @@ var woordEnDefinities = JSON.parse(woordJSON);
 function definitieEnWoord(){
     
     var arrayAantallen = Array.from(Array(woordEnDefinities.length).keys());
-    console.log(arrayAantallen);
-
+    // console.log(arrayAantallen);
+    var woorden = [];
+    var definities = [];
 
     for (var x = 0; x < 4; x++) {
         var random = Math.floor(Math.random() * arrayAantallen.length);
-        console.log(woordEnDefinities[arrayAantallen[random]].woord);
+        definities.push(woordEnDefinities[arrayAantallen[random]].definitie);
+        woorden.push(woordEnDefinities[arrayAantallen[random]].woorden);
         document.getElementById("woord" + (x+1)).innerHTML = woordEnDefinities[arrayAantallen[random]].woord;
         arrayAantallen.splice(random, 1);
 
         // console.log(x, random, arrayAantallen);
     }
-
+    var random = Math.floor(Math.random() * definities.length);
+    // console.log(woorden);
+    // console.log(definities);
+    console.log(random);
+    document.getElementById("deDefinitie").innerHTML = definities[random];
  }
 
 
