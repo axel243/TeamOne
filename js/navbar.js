324dba433f26f4f06d04cd8caba2f566ea59349c
juless89 <j.m.r.kramer@gmail.com>
2019-10-11 16:02:32 +0200

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -6,6 +6,6 @@
 // });
 
 $(Document).ready(function () {
-    var string = '<navid="navigatie"><ul><li><!--linkone--><a href="index.html">Startgame</a></li><li><!--linktwo--><a href="#inputwoord.html">Inputwoord</a></li><li><!--linkthree--><a href="hetSpel.html">Woordenlijst</a></li><li><!--linkfour--><a href="#aboutUs">Aboutus</a></li></ul></nav>'
+    var string = '<navid="navigatie"><ul><li><!--linkone--><a href="index.html">Startgame</a></li><li><!--linktwo--><a href="inputwoord.html">Inputwoord</a></li><li><!--linkthree--><a href="hetSpel.html">Woordenlijst</a></li><li><!--linkfour--><a href="#aboutUs">Aboutus</a></li></ul></nav>'
     $("#navbars").html(string);
 });
