21829cfc0a4fbc679f4725f704702764f6cf75e8
Daniëlle Schrijver <job_tr@hotmail.com>
2019-10-11 11:12:19 +0200

    koppeling naam

--- js/spelernaam.js
+++ js/spelernaam.js
@@ -1,6 +1,9 @@
 function spelernaam(){
     var speler = localStorage.getItem("deSpelers");
      var gangster = JSON.parse(speler);
-     console.log(gangster);
+     console.log(gangster.naam); 
+     var welkomstdiv = document.getElementById("welkomsttekst");
+     console.log(welkomstdiv);
+     welkomstdiv.innerHTML = "Hoi, en welkom bij dit toffe spel " + gangster.naam;
 
 }
